77dd3c7c5d633ed026e602453e9c4caa01efef9e
Sam Gilman <samuelgilman@gmail.com>
2011-05-18 20:31:19 -0500

    added jquery

---
+++ public/javascripts/contribute.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+	// Inform Mobile Safari Users About No Uploading//
+	
+	if (navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) ) {
+	 	alert('Sorry! Apple does not allow uploads from Mobile Safari. Try a PC/Mac browser.')
+	}
+	
+	//Auto Complete Word Searching//
+	
+	//Showing And Hiding Divs//
+});
