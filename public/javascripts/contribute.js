8c5117f2e6fa7309f747f1948f16744afca1c82a
Sam Gilman <samuelgilman@gmail.com>
2011-05-21 20:47:34 -0500

    added readme to test both git pushes

--- public/javascripts/contribute.js
+++ public/javascripts/contribute.js
@@ -1,9 +1,12 @@
 jQuery(document).ready(function() {
+	
 	// Inform Mobile Safari Users About No Uploading//
 	
 	if (navigator.userAgent.match( /(iPod|iPhone|iPad)/ ) ) {
 	 	alert('Sorry! Apple does not allow uploads from Mobile Safari. Try a PC/Mac browser.')
 	}
+	
+	// Disable this and check when Internet is up //
 	
 	$('#image_terms_and_conditions').click(function() {
 		if ($('#image_terms_and_conditions').attr('checked')) {
