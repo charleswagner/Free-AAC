cb7f4fbf774d84c02e5825a05d9b7a06d2e0db89
Sam Gilman <samuelgilman@gmail.com>
2011-05-23 17:50:27 -0500

    got soundmanager working, see if it works on iPad

--- public/javascripts/sound.js
+++ public/javascripts/sound.js
@@ -1,55 +1,75 @@
 jQuery(document).ready(function() {
 	
-	var audio = undefined;
+	soundManager.url = '/data/swf/';
+	soundManager.flashVersion = 9; // optional: shiny features (default = 8)
+	soundManager.useFlashBlock = true; // optionally, enable when you're ready to dive in
+	soundManager.debugMode = false;
+	// enable HTML5 audio support, if you're feeling adventurous. iPad/iPhone will always get this.
+	// soundManager.useHTML5Audio = true;
+	soundManager.onready(function(oStatus) {
+	    soundManager.url = '/data/swf/';
+	  if (!oStatus.success) {
+	    return false;
+	  }
+	  // soundManager is initialised, ready to use. Create a sound for this demo page.
+	/*
+	if (soundManager.flashVersion > 8) {
+	t = window.setInterval(checkRAM,500);
+	document.getElementById('flash-ram-use').style.display = 'inline';
+	checkRAM();
+	}*/
 
+	 
+
+	 soundManager.onload = function() {
+	  // soundManager is ready to use.
+	  // createSound() / play() etc. can now be called
+
+
+
+	    //
+	    //soundManager.play('i','/data/i.wav');
+	}
+	soundManager.onerror = function() {
+	  // Oh no! No sound support.
+	  // Maybe configure your app to ignore sound calls.
+	  // (SM2 calls will silently return false after this point.)
+	}
+
+	  //soundManager.play('i');
+	 // soundManager.play('love');
+	  //soundManager.play('you');
+	 // soundManager.play('aDrumSound');
+	});
+	
+
+	
 	//Attempt to fix iPad click function issue
 	//http://www.danwellman.co.uk/fixing-jquery-click-events-for-the-ipad/ 
 
 	var ua = navigator.userAgent,
 	event = (ua.match(/iPad/i)) ? "touchstart" : "click";
 
-	//preload sounds
-	
-	$('.image').each(function(index) {
-		soundPlay(this.id, false)
-	});
+	//onclick function to play sound 
 
-	//play sound
-	
 	$('.image').bind(event, function(e){
-		if (audio == undefined) {
-			var audio = new Audio("/sounds/"+ this.id + ".mp3");			
+		
+		if (soundManager.getSoundById(this.id)) {
+			soundManager.play(this.id);
+			
+			
+		}	else {
+			soundManager.createSound({
+			  id: this.id,
+			  url: "/sounds/"+this.id+".mp3",
+			  autoLoad: true,
+			  autoPlay: true,
+			  volume: 50
+			});
 		}
-		audio.play();		
-	})
 
-	//http://stackoverflow.com/questions/187098/cross-platform-cross-browser-way-to-play-sound-from-javascript//
-	//======================================================================
-	var soundEmbed = null;
-	//======================================================================
-	function soundPlay(which, autostart)
-	    {
-	    if (!soundEmbed)
-	        {
-	        soundEmbed = document.createElement("embed");
-	        soundEmbed.setAttribute("src", "/sounds/"+which+".mp3");
-	        soundEmbed.setAttribute("hidden", true);
-	        soundEmbed.setAttribute("autostart", autostart);
-		    soundEmbed.setAttribute("id", which);
-	        }
-	    else
-	        {
-	        document.body.removeChild(soundEmbed);
-	        soundEmbed.removed = true;
-	        soundEmbed = null;
-	        soundEmbed = document.createElement("embed");
-	        soundEmbed.setAttribute("src", "/sounds/"+which+".mp3");
-	        soundEmbed.setAttribute("hidden", true);
-	        soundEmbed.setAttribute("autostart", autostart);
-	        }
-	    soundEmbed.removed = false;
-	    document.body.appendChild(soundEmbed);
-	    }
-	//======================================================================
+	})
+	
+	
 
 })
