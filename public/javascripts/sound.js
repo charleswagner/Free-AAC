aa65a42196ac5db4a54963e28789cbe0bb009882
Sam Gilman <samuelgilman@gmail.com>
2011-05-24 02:11:00 -0500

    phrase play almost complete

--- public/javascripts/sound.js
+++ public/javascripts/sound.js
@@ -2,7 +2,40 @@ jQuery(document).ready(function() {
 	soundManager.url = '/data/swf/';
 	soundManager.flashVersion = 9; // optional: shiny features (default = 8)
 	soundManager.useFlashBlock = true; // optionally, enable when you're ready to dive in
-	soundManager.debugMode = false; // enable HTML5 audio support, iPad/iPhone will always get this.
+	soundManager.debugMode = false; // enable HTML5 audio support, iPad/iPhone will always get this.	
+	$('#play').click(function(){
+		if ($('#phrase img').length > 0) {
+			playPhrase();
+		}
+	})
+	
+	function playPhrase() {
+		var spoken_phrase = "";
+		var images = $('#phrase img');
+		images.fadeOut('fast')
+		$('#play').css({'top' : '15px', 'background-image' : 'url(/images/loading.gif)'})
+		$('#phrase img').each(function(index){
+			spoken_phrase += (" " + this.id);
+		})
+		if (soundManager.getSoundById(spoken_phrase)) {
+			soundManager.play(spoken_phrase);
+		}	else {
+			soundManager.createSound({
+			  id: spoken_phrase,
+	  		  url: "http://charleswagner.net/speak/"+spoken_phrase+".mp3",
+			  autoLoad: true,
+			  autoPlay: true,
+			  volume: 50,
+			  whileplaying: function() {
+			  	$('#play').css({'background-image' : 'url(/images/play.png)', 'top' : '5px'});
+				images.remove();
+			  }
+			});
+		}
+	}	
+	
+	/* 
+	
 	//soundManager.useHTML5Audio = true;
 	soundManager.onready(function(oStatus) {
 		soundManager.url = '/data/swf/';
@@ -17,38 +50,8 @@ jQuery(document).ready(function() {
 		}
 	});
 	
-	$('#play').click(function(){
-		if ($('#phrase img').length > 0) {
-			playPhrase();
-		}
-	})
-	
-	function playPhrase() {
-		var spoken_phrase = "";
-
-		$('#phrase img').fadeOut('fast')
-		$('#play').css({'top' : '15px', 'background-image' : 'url(/images/loading.gif)'})
-
-		$('#phrase img').each(function(index){
-			spoken_phrase += (" " + this.id);
-		})
-
-		if (soundManager.getSoundById(spoken_phrase)) {
-			soundManager.play(spoken_phrase);
-		}	else {
-			soundManager.createSound({
-			  id: spoken_phrase,
-	  		  url: "http://charleswagner.net/speak/"+spoken_phrase+".mp3",
-			  autoLoad: true,
-			  autoPlay: true,
-			  volume: 50,
-			  whileplaying: function() {
-			  	$('#play').css({'background-image' : 'url(/images/play.png)', 'top' : '5px'});
-				$('#phrase img').remove();
-			  }
-			});
-		}
-	}
-	
+	*/
 	
 })
+
+
